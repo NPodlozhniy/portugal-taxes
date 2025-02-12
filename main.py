54425a83d42b3d7f4afcf6537799cd2c9a2e03d9
NPodlozhniy <n.podlozhniy@zelf.co>
2025-02-12 12:25:50 +0000

    Add income category B logic

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ parser = argparse.ArgumentParser(
     description="Portugal net income calculator for 2023"
     )
 
-group = parser.add_mutually_exclusive_group(required=False)
-
 parser.add_argument(
     "income",
     action="store",
@@ -15,40 +13,74 @@ parser.add_argument(
     metavar="<income>",
     type=float,
 )
-group.add_argument(
+
+residence_group = parser.add_argument_group("residence options", "what kind of a residence status you have")
+residence_exclusive_group = residence_group.add_mutually_exclusive_group(required=False)
+
+residence_exclusive_group.add_argument(
     "-nr",
     "--non-resident",
     action="store_const",
     const="nr",
-    help="specify if you are a non-resident",
+    help="specify if you are not a resident",
 )
-group.add_argument(
+residence_exclusive_group.add_argument(
     "-nhr",
     "--non-habitual",
     action="store_const",
     const="nhr",
     help="specify if you are a non-habitual resident",
 )
-group.add_argument(
+residence_exclusive_group.add_argument(
     "-r",
     "--residence",
     action="store",
-    help="specify the region of residence if any",
+    help="specify the region of residence",
     metavar="<region>",
 )
+
+category_group = parser.add_argument_group("income categories", "what is the income category: A or B")
+category_exclusive_group = category_group.add_mutually_exclusive_group(required=True)
+
+category_exclusive_group.add_argument(
+    "-a",
+    "--regular-employee",
+    action="store_true",
+    help="specify if you have income category A",
+)
+
+category_exclusive_group.add_argument(
+    "-b",
+    "--independent-worker",
+    action="store",
+    help="specify the month and date when activity was opened",
+    metavar="<activity opened at>",
+)
+
+category_group.add_argument(
+    "-e",
+    "--activity-expenses",
+    action="store",
+    help="specify the amount of business related expenses",
+    metavar="<activity expenses>",
+    type=float,
+    default=0,
+)
+
 args = parser.parse_args()
 
 if __name__ == "__main__":
 
     residence = args.non_resident or args.non_habitual
     region = args.residence
+    opened_at = args.independent_worker
 
     if residence:
-        income = Income(args.income, residence=residence)
+        income = Income(args.income, residence=residence, opened_at=opened_at, expenses=args.activity_expenses)
     elif region:
-        income = Income(args.income, region=region)
+        income = Income(args.income, region=region, opened_at=opened_at, expenses=args.activity_expenses)
     else:
-        income = Income(args.income)
+        income = Income(args.income, opened_at=opened_at, expenses=args.activity_expenses)
 
     print(f"\n{income}\n")
 
--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 from enum import Enum
 import numpy as np
 
+from datetime import datetime
+
 class Thresholds(Enum):
     Mainland = [7479, 11284, 15992, 20700, 26355, 38632, 50483, 78834]
     Madeira = [7479, 11284, 15992, 20700, 26355, 38632, 50483, 78834]
@@ -28,34 +30,76 @@ class Income():
         The region of residence
     """
 
-    def __init__(self, income=None, residence='r', region='Mainland'):
+    def __init__(
+        self,
+        income: float = None,
+        residence: str = 'r',
+        region: str = 'Mainland',
+        opened_at: str = None,
+        expenses: float = None,
+    ) -> None:
         if income == None:
             raise ValueError(
                 "Specify the Annual gross income"
             )
-        self.income = income
+        else:
+            self.income = income
         if residence not in {'r', 'nr', 'nhr'}:
             raise ValueError(
                 "Incorrect type of residence, "
                 "should be one of the following: r, nr, nhr}"
             )
-        self.residence = residence
+        else:
+            self.residence = residence
         if region not in {'Mainland', 'Madeira', 'Azores'}:
             raise ValueError(
                 "Incorrect region of residence, "
                 "should be one of the following: Mainland, Madeira, Azores"
             )
-        self.region = region
-    
+        else:
+            self.region = region
+
+        if not opened_at:
+            self.category = 'A' # regular employee
+        else:
+            self.category = 'B' # independent worker (TI/ENI)
+            try:
+                self.opened_at = datetime.strptime(opened_at, '%m/%y')
+                self.activity_expenses = 0 if not expenses else float(expenses)
+            except ValueError:
+                raise ValueError(
+                "Incorrect activity opened month or expenses format, "
+                "should be a date in `mm/yy` and a float value respectively"
+            )
+
+    @property
+    def taxable_base(self) -> float:
+        """
+        Consider only TI providing services where standard taxable base - 75%
+        For ENI supplies of goods the coeeficient is different.
+        """
+        if self.category == "B":
+            # first two years of atividade with discount
+            extra_discount = (
+                0.5 if self.opened_at.year == 2023 else 0.25 if self.opened_at.year == 2022 else 0
+            )
+            # 15% are added as the discount of 75% reflects the costs for business
+            # social security and other TI related costs are deducted, so it may be reduced to zero
+            not_incurred_expenses = max(0, self.income * 0.15 - max(4104, self.social_security_tax) - self.activity_expenses)
+            return self.income * 0.75 * (1 - extra_discount) + not_incurred_expenses
+        else:
+            return max(0, self.income - max(4104, self.social_security_tax))
+
     @property
     def income_tax(self) -> float:
         if self.residence == "nr":
+           # social security payments discount is for residents only
            return self.income * 0.25
         elif self.residence == "nhr":
-            return max(0, self.income - max(4104, self.social_security_tax)) * 0.20
+            return self.taxable_base * 0.20
         else:
             return self.progressive_taxation(
-                max(0, self.income - max(4104, self.social_security_tax)),
+                self.taxable_base,
                 Thresholds[self.region].value,
                 Rates[self.region].value
             )
@@ -68,7 +112,14 @@ class Income():
 
     @property
     def social_security_tax(self) -> float:
-        return round(self.income * 0.11, 2)
+        if self.category == "B":
+            tax_year_end_at = datetime.strptime('01/24', '%m/%y')
+            months_sice_opened = tax_year_end_at.month - self.opened_at.month + 12 * (tax_year_end_at.year - self.opened_at.year)
+            # first 12 months after opening the social security is not paid
+            invoiced_months = min(12, max(0, months_sice_opened - 12))
+            return round(self.income * (invoiced_months / 12) * 0.1125, 2)
+        else:
+            return round(self.income * 0.11, 2)
 
     @staticmethod
     def progressive_taxation(income: float, thresholds: list, rates: list) -> float:
@@ -100,4 +151,8 @@ class Income():
             'nr': 'Non-resident',
             'nhr': 'Non-Habitual Resident'
             }
-        return f"<Portugal taxes for a {type[self.residence]} living in {self.region}>"
+        return " ".join([
+            f"<Portugal taxes for a {type[self.residence]}",
+            f"living {'on ' + self.region if self.residence == 'r' else 'anywhere'}",
+            f"from {'regular employment' if self.category == 'A' else 'independent provision of services'}>"
+        ])
